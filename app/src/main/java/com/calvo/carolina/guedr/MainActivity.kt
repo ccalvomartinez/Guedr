6069b9a193d5c25b9333edbe64b4a8bf86f41c54
Carolina Calvo <pececillu@gmail.com>
2017-11-01 15:47:49 +0100

    Logs

--- app/src/main/java/com/calvo/carolina/guedr/MainActivity.kt
+++ app/src/main/java/com/calvo/carolina/guedr/MainActivity.kt
@@ -2,11 +2,20 @@ package com.calvo.carolina.guedr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = MainActivity::class.java.canonicalName!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v(TAG, "Después de onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "despues de onSaveInstance")
     }
 }
