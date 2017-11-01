f2d25b2a320c9bb7db3a31740e57630a7cd796af
Carolina Calvo <pececillu@gmail.com>
2017-11-01 16:57:45 +0100

    Adding resource files and playing with FrameLayout

--- app/src/main/java/com/calvo/carolina/guedr/MainActivity.kt
+++ app/src/main/java/com/calvo/carolina/guedr/MainActivity.kt
@@ -6,16 +6,21 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG = MainActivity::class.java.canonicalName!!
+    private val TAG = MainActivity::class.java.canonicalName!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v(TAG, "Después de onCreate")
+        Log.v(TAG, "After of onCreate")
+        if (savedInstanceState != null)
+        {
+            Log.v(TAG, "Tha value of key is: ${ savedInstanceState["key"]}")
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        Log.v(TAG, "despues de onSaveInstance")
+        Log.v(TAG, "After of onSaveInstance")
+        outState?.putString("key", "valor")
     }
 }
